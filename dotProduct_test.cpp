1a98c000e8167a20154734d1737a5d328d9a0e31
Luc Dierick <ladierick@gmail.com>
2021-12-13 08:05:19 +0100

    forgot to change dotproduct_test

--- dotProduct_test.cpp
+++ dotProduct_test.cpp
@@ -3,27 +3,26 @@
 
 int main() {
 
-    std::complex<double> A[MAGNITUDE][GRIDPOINTS];
+    double A[MAGNITUDE][GRIDPOINTS];
     double B[GRIDPOINTS];
-    std::complex<double> out[MAGNITUDE];
+    double out[MAGNITUDE];
 
     for (int i = 0; i < GRIDPOINTS; i++) {
         B[i] = i;
         for (int j = 0; j < MAGNITUDE; j++) {
-            A[j][i] = {i*1.0, j*1.0};
+            A[j][i] = i*j;
         }
     }
 
-    for (std::complex<double> & i : out) {
-        i = {0,0};
+    for (double & i : out) {
+        i = 0;
     }
 
     dotProd(A, B, out);
 //    std::cout << "A[0][1]" << A[0][1] <<std::endl;
     std::cout << "OUT[1] is: " << out[1] << std::endl;
 
-    std::complex<double> test = {285,45};
-    if (out[1] == test) {
+    if (out[1] == 285) {
         std::cout << "Test passed! product is: " << out[1] << std::endl;
     } else {
         std::cout << "Test Failed!! product is: " << out[1] << "and should be: 285" << std::endl;
